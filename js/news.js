c58ad585fee885c52f89ff0aa63072bcb0c3942f
Jakub Starostecki <64737924+PouekDEV@users.noreply.github.com>
2021-05-18 09:59:54 +0200

    huh?

--- js/news.js
+++ js/news.js
@@ -6,8 +6,6 @@ fetch(proxyurl + "https://sgs-website-server.pouekdev.repl.co")
         }
         else{
             document.getElementById("done").style.display = "none";
-            document.getElementById("donee").style.display = "none";
-            document.getElementById("doneee").style.display = "none";
             document.getElementById("importantnewstext").innerHTML = body.news;
             document.getElementById("importantnewsimage").src = body.newsicon;
             document.getElementById("newestgametext").innerHTML = body.newgametext;
