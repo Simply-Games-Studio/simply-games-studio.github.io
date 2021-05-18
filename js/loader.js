425bdb71974ad2d0bc832ba49165d58eccfd934b
Jakub Starostecki <64737924+PouekDEV@users.noreply.github.com>
2021-05-18 09:29:45 +0200

    Finally!

---
+++ js/loader.js
@@ -0,0 +1,6 @@
+document.getElementById("all").style.display = "none";
+document.onload = loading();
+function loading(){
+    document.getElementById("all").style.display = "block";
+    document.getElementById("loader").style.display = "none";
+}
---
+++ js/news.js
@@ -0,0 +1,16 @@
+proxyurl = ""//"https://cors-anywhere.herokuapp.com/"
+fetch(proxyurl + "https://sgs-website-server.pouekdev.repl.co")
+.then(res => res.json()).then(body => {
+        if(!body) {
+            console.log("Error");
+        }
+        else{
+            document.getElementById("importantnewstext").innerHTML = body.news;
+            document.getElementById("importantnewsimage").src = body.newsicon;
+            document.getElementById("newestgametext").innerHTML = body.newgametext;
+            document.getElementById("newestgameimg").src = body.newgameimg;
+            document.getElementById("newestgameimg").setAttribute('onclick',"window.location.href='" + body.newgamelink + "'")
+            document.getElementById("yttitle").innerHTML = body.youtubetext;
+            document.getElementById("ytvideo").src = body.ytvideolink;
+        }
+    })
